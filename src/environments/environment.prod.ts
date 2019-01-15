7d366b36c35f0c341f3c1178f45caec70e3ec430
Vijay Kumar <vkumar@sustainware.co.uk>
2019-01-15 13:09:37 +0000

    prod api url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:'https://qpoc-api.azurewebsites.net/api/'
+  apiUrl:'https://7j1r1r312j.execute-api.eu-west-2.amazonaws.com/Prod/api/'
 };
